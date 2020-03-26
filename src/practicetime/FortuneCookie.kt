28d685d832c63ec9cb18cd90a09532a582e9576e
inog <kontakt@ingo-reschke.de>
2020-03-26 23:24:51 +0100

    cleanup an follow the tips of analyze
    Signed-off-by: inog <kontakt@ingo-reschke.de>

--- src/practicetime/FortuneCookie.kt
+++ src/practicetime/FortuneCookie.kt
@@ -8,17 +8,17 @@ fun main() {
 }
 
 fun getFortuneCookie(): String {
-    var fortunes : List<String> = listOf(
+    val fortunes: List<String> = listOf(
         "You will have a great day!"
-        ,"Things will go well for you today."
-        ,"Enjoy a wonderful day of success."
-        ,"Be humble and all will turn out well."
-        ,"Today is a good day for exercising restraint."
-        ,"Take it easy and enjoy life!"
-        ,"Treasure your friends because they are your greatest fortune."
-        )
+        , "Things will go well for you today."
+        , "Enjoy a wonderful day of success."
+        , "Be humble and all will turn out well."
+        , "Today is a good day for exercising restraint."
+        , "Take it easy and enjoy life!"
+        , "Treasure your friends because they are your greatest fortune."
+    )
     print("Enter your birthday:")
-    var birthday : Int = readLine()?.toIntOrNull()?: 1;
-    var index = birthday.rem(fortunes.size)
+    val birthday : Int = readLine()?.toIntOrNull()?: 1
+    val index = birthday.rem(fortunes.size)
     return fortunes.get(index)
 }
