e69e960c6a9c803661ca55a6d7bd7b0f14e2e7d2
inog <kontakt@ingo-reschke.de>
2020-03-26 21:36:49 +0100

    move dayOfWeek to Greetings class and inprove the greeting mehtod
    Signed-off-by: inog <kontakt@ingo-reschke.de>

--- src/AMS.kt
+++ src/AMS.kt
@@ -2,19 +2,18 @@ import java.util.*
 
 
 fun main() {
-    dayOfWeek()
+    feedTheFish()
 }
 
-fun dayOfWeek(){
-    println("What day is today?")
-    val dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
-    when (dayOfWeek) {
-        1 -> print("Sunday ")
-        2 -> print("Monday")
-        3 -> print("Tuesday")
-        4 -> print("Wednesday")
-        5 -> print("Thursday")
-        6 -> print("Friday")
-        7 -> print("Saturday")
-    }
+
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay() : String {
+    val week = listOf<String>("Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday", "Sunday")
+    return week[Random().nextInt(7)]
 }
--- src/Greetings.kt
+++ src/Greetings.kt
@@ -1,5 +1,31 @@
+import java.time.LocalTime
+import java.util.*
 
-//input param 16
 fun main(args:Array<String>) {
-    println("Good ${if (args[0].toInt() < 12) "Morning," else "Night,"} Kotlin")
+    greet()
+    dayOfWeek()
 }
+
+fun greet() {
+    var hour = LocalTime.now().hour
+    when(hour){
+        in 0..12 -> println("Good morrning")
+        in 12..18 -> println("Good afternoon")
+        in 18..23 -> println("Good evening")
+    }
+}
+
+fun dayOfWeek() {
+    println("What day is today?")
+    val dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+    when (dayOfWeek) {
+        1 -> print("Sunday")
+        2 -> print("Monday")
+        3 -> print("Tuesday")
+        4 -> print("Wednesday")
+        5 -> print("Thursday")
+        6 -> print("Friday")
+        7 -> print("Saturday")
+    }
+}
+
