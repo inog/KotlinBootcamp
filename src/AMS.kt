663a9458b2b6b5931a57bb2804e10d1185e4fbf7
inog <kontakt@ingo-reschke.de>
2020-03-26 16:07:48 +0100

    Initial commit

---
+++ src/AMS.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+fun main() {
+    dayOfWeek()
+}
+
+fun dayOfWeek(){
+    println("What day is today?")
+    val dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+    when (dayOfWeek) {
+        1 -> print("Sunday ")
+        2 -> print("Monday")
+        3 -> print("Tuesday")
+        4 -> print("Wednesday")
+        5 -> print("Thursday")
+        6 -> print("Friday")
+        7 -> print("Saturday")
+    }
+}
